c803854875665a40f525be9478f6fc6bcad5931b
abstark-3000 <149358036+abstark-3000@users.noreply.github.com>
2026-01-27 18:50:12 +0530

    Fix database config and create tables on startup

--- app.py
+++ app.py
@@ -9,9 +9,24 @@ app.secret_key = "supersecretkey"
 # ======================
 # DATABASE SETUP
 # ======================
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blood_app.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blood_app.db'
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# db = SQLAlchemy(app)
+
+import os
+
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
+    "DATABASE_URL",
+    "sqlite:///" + os.path.join(BASE_DIR, "blood_app.db")
+)
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
 db = SQLAlchemy(app)
+
+
+
 
 # ======================
 # MODELS
